322a93475b03550c81af6b46495bf5af7badfc71
vmurali100 <vmurali100@users.noreply.github.com>
2024-02-28 08:33:13 +0530

    Adding Quiz

--- app.js
+++ app.js
@@ -1,56 +1,76 @@
+function populate() {
+  if (quiz.isEnded()) {
+    showScores();
+  } else {
+    //Show Question
 
-function populate(){
-	if(quiz.isEnded()){
-		showScores()
-	}
-	else{
-		//Show Question
+    var element = document.getElementById("question");
+    element.innerHTML = quiz.getQuestionIndex().text;
 
-		var element=document.getElementById("question")
-		element.innerHTML=quiz.getQuestionIndex().text;
+    //Show Choices
 
-		//Show Choices
+    var choices = quiz.getQuestionIndex().choice;
 
-		var choices=quiz.getQuestionIndex().choice;
-
-		for (i=0;i<choices.length;i++){
-			var element = document.getElementById("choice"+i)
-			element.innerHTML=choices[i];
-			guess("btn"+i,choices[i])
-		}
-		showProgress()
-	}
+    for (i = 0; i < choices.length; i++) {
+      var element = document.getElementById("choice" + i);
+      element.innerHTML = choices[i];
+      guess("btn" + i, choices[i]);
+    }
+    showProgress();
+  }
 }
 
-function showProgress(){
-	var currentQuestionNumber=quiz.questionIndex+1;
-	var element=document.getElementById("progress");
-	element.innerHTML= "Quesion "+ currentQuestionNumber +" of " + quiz.questions.length;
+function showProgress() {
+  var currentQuestionNumber = quiz.questionIndex + 1;
+  var element = document.getElementById("progress");
+  element.innerHTML =
+    "Quesion " + currentQuestionNumber + " of " + quiz.questions.length;
 }
 
-function guess(id,guess){
-	var button=document.getElementById(id);
-	button.onclick=function (){
-		quiz.guess(guess);
-		populate();
-	}
+function guess(id, guess) {
+  var button = document.getElementById(id);
+  button.onclick = function () {
+    quiz.guess(guess);
+    populate();
+  };
 }
-function showScores(){
-	var gameOverHtml="<h1>Result</h1>";
-	gameOverHtml+="<h2 id='score'>Your Score is "+quiz.score+"</h2>";
-	var element=document.getElementById("quiz");
+function showScores() {
+  var gameOverHtml = "<h1>Result</h1>";
+  gameOverHtml += "<h2 id='score'>Your Score is " + quiz.score + "</h2>";
+  var element = document.getElementById("quiz");
 
-	element.innerHTML=gameOverHtml
+  element.innerHTML = gameOverHtml;
 }
 
-var questions=[
-new Question("Which is not An Object Oriented Programming Language ? ", ["C","C++","Java","C"],"C"),
-new Question("Which Language is Used for styling the webpages ? ", ["HTML","CSS","Java","Jquery"],"CSS"),
-new Question("THere are _ main components in OOP Language ? ", ["1","2","3","4"],"4"),
-new Question("Which Language is used for Web apps ? ", ["PHP","PYthon++","Java","All"],"All"),
-new Question("Which is MVC ? ", ["Language","Library","Framework","All"],"Framework"),
-]
+var questions = [
+  new Question(
+    "Which is not An Object Oriented Programming Language ? ",
+    ["C", "C++", "Java", "C"],
+    "C"
+  ),
+  new Question(
+    "Which Language is Used for styling the webpages ? ",
+    ["HTML", "CSS", "Java", "Jquery"],
+    "CSS"
+  ),
+  new Question(
+    "THere are _ main components in OOP Language ? ",
+    ["1", "2", "3", "4"],
+    "4"
+  ),
+  new Question(
+    "Which Language is used for Web apps ? ",
+    ["PHP", "PYthon++", "Java", "All"],
+    "All"
+  ),
+  new Question(
+    "Which is MVC ? ",
+    ["Language", "Library", "Framework", "All"],
+    "Framework"
+  ),
+];
 
-var quiz= new Quiz(questions);
-
-populate();
+var quiz = new Quiz(questions); // This is line which is Going to be Excuted First . Step 1 .
+// with prototype we have added 3 methods , isEnded, guess ,getQuestionIndex();
+console.log(quiz)
+populate(); // Will display all the Questions
